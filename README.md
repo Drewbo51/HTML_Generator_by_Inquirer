67df68b1cf94c8437e14386277d7fe18ca67363b
Drewbo51 <teylorsdad51@gmail.com>
2021-09-26 03:15:20 -0700

    addedFolderPNGS

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # HTML_Generator_by_Inquirer
+
+1. Initially I fixed the tests and/or classes. In each of the classes the getRole() method returned a class object where the test was looking for a string matching the name of the class. Also there were a few other issues with the tests that I resolved and I changed the "Test" to "it" as well as adding "Describe".
+
+2. Using vanilla Javascript I was unable to remove a class that was attached in "main.html" that was called "justify-content-center" so I just went into the template and removed the hard coding of it.
+
+3. I wasn't able to dynamically create the "style.css" the way I wanted to, so I created a folder called "style" with a css stylesheet created in it. Then I created a method that used the "fs" to read and write a new "style.css" file that would be created and added to the output folder at the same time as the team list was created.(**code to create sheets below**)
+
+```m
+const styleDir = path.resolve(__dirname, "./style");
+const cssOnPage = fs.readFileSync(path.resolve(styleDir, "style.css"), "utf-8");
+
+const outputStyling = path.join(OUTPUT_DIR, "style.css");
+fs.writeFileSync(outputStyling, cssOnPage, "utf-8");
+```
+
+
+4.I was unable to create the video of the walkthrough so I am attaching pictures of me using the inquirer to create the document.
+
+![](./readMEpngs/beginningPhoto.png)
+
+
+
+![](./readMEpngs/middleStage.png)
+
+
+
+![](./readMEpngs/developTeamStage.png)
+
+
+
+![](./readMEpngs/generatedHTML.png)
+
+
+
+![](./readMEpngs/deployedState.png)
+
+
+
+![](./readMEpngs/deployedState2.png)
--- app.js
+++ app.js
@@ -77,7 +77,7 @@ function askEngineer() {
     ]).then((answers) => {
                         const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
                         teamMembers.push(engineer);
-                        answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : buildTeam()
+                        answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : console.log("Building Team... please check output folder\n\n"), buildTeam()
     })
     .catch((err) => console.error(err));
 }
@@ -113,64 +113,25 @@ function askIntern() {
     ]).then((answers) => {
                         const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
                         teamMembers.push(intern);
-                        answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : buildTeam()
+                        answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : console.log("Building Team... please check output folder\n\n"), buildTeam()
     })
     .catch((err) => console.error(err));
-}
-      
-        
+}        
 
 createManager()
             .then((answers) => {
             const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice);
             teamMembers.push(manager);
-            answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : buildTeam()
+            answers.roleSelect === "Engineer" ? askEngineer() : answers.roleSelect === "Intern" ? askIntern() : console.log("Building Team... please check output folder\n\n"), buildTeam()
     })
     .catch((err) => console.error(err));
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-        // const OUTPUT_DIR = path.resolve(__dirname, "output");
-        // const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-        // function buildTeam() {
-        //     if (!fs.existsSync(OUTPUT_DIR)) {
-        //         fs.mkdirSync(OUTPUT_DIR)
-        //     }
-        //     fs.writeFileSync(OUTPUT_DIR, render(teamMembers), "utf-8")
-        // }
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-//build team
 function buildTeam() {
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
     }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-    fs.writeFileSync(outputStyling, cssOnPage, "utf-8")
-    //fs.writeFileSync(OUTPUT_DIR, render(teamMembers), "utf-8")
-    
+    fs.writeFileSync(outputStyling, cssOnPage, "utf-8")   
 }
 
-// start()
